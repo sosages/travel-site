8e77fbcb6af2ed0e24dd6d763b480cfb7ab5316d
steve <scwillcox@gmx.com>
2018-02-20 00:13:26 +0000

    updated gulfile and added styles.css

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,22 @@ gulp.task('html', function () {
   console.log('Imagine something useful');
 });
 
+gulp.task('styles', function () {
+  console.log('Changing the CSS');
+});
+
+
 
 gulp.task('watch', function () {
-  
+
   watch('./app/index.html', function () {
     gulp.start('html');
   });
+
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('styles');
+  });
+
+
 
 });
